1daf90cb6df9b096e1966c9132d19c19256fcf21
bytemedarling <jessicakostyk@outlook.com>
2026-03-09 01:31:05 -0400

    fixing hamburger toggle and nav links

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+/*Hamburger Menu */
+const menuButton = document.getElementById("menuButton");
+const mainNav = document.getElementById("mainNav");
+
+menuButton.addEventListener("click", () => {
+  mainNav.classList.toggle("open");
+});
+
 /* Enter Button */
 document.getElementById("enterBtn").addEventListener("click", () => {
   document.getElementById("about").scrollIntoView({ behavior: "smooth" });
